bcaf428e9ee2727a21692dbf60853efb9a9051a5
ryadovoykomissarov <edikermolenkog@gmail.com>
2024-08-25 00:43:33 +0300

    fix buttons

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -10,13 +10,11 @@ callback_router = Router()
 async def category_cb_handler(call: CallbackQuery):
     await call.answer()
     category = call.data.split("_")[1] # type: ignore
-    print(category)
     channels = get_channels_by_cat(category)
     print(channels)
-    message = ""
+    message = "<b>"+category+"</b>\n"
     for ch in channels:
-        row = "<a href='"+ch['link']+"'>"+ch['name']+"</a>\n" + "\nСтоимость рекламы: " + ch['price']+ "\n\n"
-        print(row)
+        row = "<a href='"+ch['link']+"'>"+ch['name']+"</a>\n" + "Стоимость рекламы: " + ch['price']+ "\n\n"
         message = message+row
     
     await call.message.answer(text=message, parse_mode='HTML', link_preview_options=LinkPreviewOptions(is_disabled=True)) # type: ignore
--- keyboards/inline_kbs.py
+++ keyboards/inline_kbs.py
@@ -6,7 +6,7 @@ def get_categories_keyboard(categories):
     builder = InlineKeyboardBuilder()
     for category in categories:
         ctg_key = "category_"+category
-        builder.button(text=category, callback_data=ctg_key)
+        builder.row(text=category, callback_data=ctg_key)
     
     kb_inline = builder.as_markup()
     return kb_inline
