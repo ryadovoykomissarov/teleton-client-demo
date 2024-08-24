51a48c84d39b1aac5a6025e5f84adabd32f6e3d2
ryadovoykomissarov <edikermolenkog@gmail.com>
2024-08-25 00:56:24 +0300

    keyboards fix

--- keyboards/inline_kbs.py
+++ keyboards/inline_kbs.py
@@ -3,11 +3,13 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def get_categories_keyboard(categories):
+    buttons = []
     builder = InlineKeyboardBuilder()
     for category in categories:
         ctg_key = "category_"+category
-        builder.row(text=category, callback_data=ctg_key)
+        buttons.append(InlineKeyboardButton(text=category, callback_data=ctg_key))
     
+    builder.row(buttons)
     kb_inline = builder.as_markup()
     return kb_inline
     
